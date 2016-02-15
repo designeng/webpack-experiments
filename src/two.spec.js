4c9ed8b662aa1c9f908f9f43e3dbb13944af3732
designeng <denissavenok@gmail.com>
2016-02-15 18:03:36 +0300

    code cleaned

--- src/two.spec.js
+++ src/two.spec.js
@@ -3,7 +3,6 @@ const createNotResolvedPlugin = (options) => {
         factories: {
             createNotResolved(resolver, compDef, wire){
                 resolver.reject("NO RESOLVED")
-                // resolver.resolve("RESOLVED")
             }
         }
     }
