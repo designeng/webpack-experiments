1c83bdfebf161041e9dd0427d8a8bf286cb56ee1
designeng <denissavenok@gmail.com>
2016-02-20 10:27:50 +0300

    pipelined

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
 import wire         from 'essential-wire';
+import pipeline     from 'when/pipeline';
 import mainSpec     from './main.spec';
-
 import expressSpec  from './express.spec';
-
-import Timer    from './utils/timer';
+import Timer        from './utils/timer';
 
 let timer = new Timer();
 
-wire(mainSpec).then(context => {
-    context.wire(expressSpec).then(context => {
-        console.log("WIRING TIME:", timer.end());
-    }).otherwise(error => console.error("ERROR expressSpec:", error));
-}).otherwise(error => console.error("ERROR mainSpec:", error));
+
+const mainTask = () => {
+    return wire(mainSpec);
+}
+
+const expressTask = (context) => {
+    console.log("context", context);
+    return context.wire(expressSpec);
+}
+
+pipeline([mainTask, expressTask]).then(context => {
+    console.log("WIRING TIME:", timer.end());
+}).otherwise(error => console.error("ERROR:", error));
