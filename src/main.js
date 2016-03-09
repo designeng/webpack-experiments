876bf5283e8133ac72a405f2dfc71bba431b4853
designeng <denissavenok@gmail.com>
2016-03-09 18:47:25 +0300

    main tasks

--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 import wire         from 'essential-wire';
 import pipeline     from 'when/pipeline';
 
-import demoSpec     from './pages/demo/page.spec';
 import expressSpec  from './express.spec';
 
 import Timer    from './utils/timer';
 
 let timer = new Timer();
 
-const demoTask = (context) => {
+const expressTask = (context) => {
     return wire(expressSpec);
 }
 
-const expressTask = (context) => {
-    return context.wire(demoSpec);
-}
-
-pipeline([demoTask, expressTask]).then(context => {
+pipeline([expressTask]).then(context => {
     timer.end();
 }).otherwise(error => console.error("ERROR:::", error));
