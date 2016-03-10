c6e8049318af8b37e1d77195d1ee90e90b425be3
designeng <denissavenok@gmail.com>
2016-03-10 15:27:05 +0300

    shorted

--- test/smoke/index.js
+++ test/smoke/index.js
@@ -12,12 +12,10 @@ const smokeTask = () => {
 
 pipeline([smokeTask]).then(context => {
     let $ = cheerio.load(context.page);
-    let rootItemElementCollection = $('.nncard');
-    expect(rootItemElementCollection.length).to.equal(13);
-
-    let item = $('#56e1472aec05c4436b000a16');
-    let caption = item.find('h4').text();
+    let collection = $('.nncard');
+    expect(collection.length).to.equal(13);
+ 
+    let caption = $('#56e1472aec05c4436b000a16').find('h4').text();
     let str = 'Российский офис Subaru';
-    let strLength = str.length;
-    expect(caption.slice(0, strLength)).to.equal(str);
+    expect(caption.slice(0, str.length)).to.equal(str);
 }).otherwise(error => console.error("ERROR:::", error));
