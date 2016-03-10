e85a891b93b0c2936fc45e8f0282ce7c53a48d57
designeng <denissavenok@gmail.com>
2016-03-10 15:11:59 +0300

    added smoke expectations - run

--- src/smoke.js
+++ src/smoke.js
@@ -1,12 +1,24 @@
 import wire         from 'essential-wire';
 import pipeline     from 'when/pipeline';
 
+import chai,  { expect }  from 'chai';
+import cheerio      from 'cheerio';
+
 import demoPageSpec from './pages/demo/page.spec';
+
 
 const smokeTask = () => {
     return wire(demoPageSpec);
 }
 
 pipeline([smokeTask]).then(context => {
-    console.log("CTX::::", context);
+    let $ = cheerio.load(context.page);
+    let rootItemElementCollection = $('.nncard');
+    expect(rootItemElementCollection.length).to.equal(13);
+
+    let item = $('#56e1472aec05c4436b000a16');
+    let caption = item.find('h4').text();
+    let str = 'Российский офис Subaru';
+    let strLength = str.length;
+    expect(caption.slice(0, strLength)).to.equal(str);
 }).otherwise(error => console.error("ERROR:::", error));
