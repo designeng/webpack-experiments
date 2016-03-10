009a4f06526c04a5447937aa6514467fd2b07023
designeng <denissavenok@gmail.com>
2016-03-10 15:31:05 +0300

    with chalk

--- test/smoke/index.js
+++ test/smoke/index.js
@@ -3,6 +3,7 @@ import pipeline     from 'when/pipeline';
 
 import { expect }   from 'chai';
 import cheerio      from 'cheerio';
+import chalk        from 'chalk';
 
 import demoPageSpec from '../../src/pages/demo/page.spec';
 
@@ -13,9 +14,9 @@ const smokeTask = () => {
 pipeline([smokeTask]).then(context => {
     let $ = cheerio.load(context.page);
     let collection = $('.nncard');
-    expect(collection.length).to.equal(13);
+    expect(collection.length).to.equal(10);
  
     let caption = $('#56e1472aec05c4436b000a16').find('h4').text();
     let str = 'Российский офис Subaru';
     expect(caption.slice(0, str.length)).to.equal(str);
-}).otherwise(error => console.error("ERROR:::", error));
+}).otherwise(error => console.error(chalk.red("ERROR:::"), chalk.blue(error)));
